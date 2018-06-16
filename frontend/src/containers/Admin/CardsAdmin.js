bbbb9e00e7a365a6840576c59879aa820b8657a5
Alban Pommeret <ap@reputationvip.com>
2018-06-16 19:53:29 +0200

    Rerender Creation Card component after Card creation

--- frontend/src/containers/Admin/CardsAdmin.js
+++ frontend/src/containers/Admin/CardsAdmin.js
@@ -3,6 +3,7 @@ import Card from "../Card/Card";
 import { Mutation, Query } from "react-apollo";
 import gql from "graphql-tag";
 import styled from "styled-components";
+import uuid from 'uuid-v4';
 
 const CardWrapper = styled.div`
   &:not(:last-of-type) {
@@ -42,9 +43,9 @@ const queryCardsAndLabels = gql`
 
 const ADD_CARD = gql`
   mutation createCard(
-    $sentence: String!
-    $answer: String!
-    $clientMutationId: String!
+  $sentence: String!
+  $answer: String!
+  $clientMutationId: String!
   ) {
     createCard(
       input: {
@@ -68,87 +69,99 @@ const ADD_CARD = gql`
   }
 `;
 
-export default () => (
-  <Query query={queryCardsAndLabels}>
-    {({ data: { cards: cardsNodes, labels }, loading }) => {
-      if (loading || !cardsNodes) return "loading";
+export default class extends React.Component {
+  state = {
+    creationKey: uuid()
+  };
 
-      const selectableLabels = labels.edges.map(({ node: label }) => label);
+  render() {
+    return (
+      <Query query={queryCardsAndLabels}>
+        {({ data: { cards: cardsNodes, labels }, loading }) => {
+          if (loading || !cardsNodes) return "loading";
 
-      const cards = cardsNodes.edges.map(
-        ({ node: { id, sentence, answer, labels } }) => ({
-          id,
-          question: sentence,
-          response: answer,
-          labels: labels.edges
-            ? labels.edges.map(({ node: label }) => label)
-            : []
-        })
-      );
+          const selectableLabels = labels.edges.map(({ node: label }) => label);
 
-      return (
-        <React.Fragment>
-          <CardWrapper>
-            <Mutation
-              mutation={ADD_CARD}
-              update={(cache, { data: { createCard } }) => {
-                createCard.labels.edges = createCard.labels.edges || [];
-                createCard.__typename = "Card";
+          const cards = cardsNodes.edges.map(
+            ({ node: { id, sentence, answer, labels } }) => ({
+              id,
+              question: sentence,
+              response: answer,
+              labels: labels.edges
+                ? labels.edges.map(({ node: label }) => label)
+                : []
+            })
+          );
 
-                const { cards, labels } = cache.readQuery({
-                  query: queryCardsAndLabels
-                });
+          return (
+            <React.Fragment>
+              <CardWrapper key={this.state.creationKey}>
+                <Mutation
+                  mutation={ADD_CARD}
+                  update={(cache, { data: { createCard } }) => {
+                    this.setState({
+                      creationKey: uuid()
+                    });
 
-                cache.writeQuery({
-                  query: queryCardsAndLabels,
-                  data: {
-                    cards: {
-                      ...cards,
-                      edges: [
-                        ...cards.edges,
-                        {
-                          node: createCard,
-                          __typename: "CardEdge"
-                        }
-                      ]
-                    },
-                    labels
-                  }
-                });
-              }}
-            >
-              {createCard => (
-                <Card
-                  initialMode="creation"
-                  handleSubmit={values => {
-                    createCard({
-                      variables: {
-                        sentence: values.question,
-                        answer: values.response,
-                        clientMutationId: "mutationId"
+                    createCard.labels.edges = createCard.labels.edges || [];
+                    createCard.__typename = "Card";
+
+                    const { cards, labels } = cache.readQuery({
+                      query: queryCardsAndLabels
+                    });
+
+                    cache.writeQuery({
+                      query: queryCardsAndLabels,
+                      data: {
+                        cards: {
+                          ...cards,
+                          edges: [
+                            ...cards.edges,
+                            {
+                              node: createCard,
+                              __typename: "CardEdge"
+                            }
+                          ]
+                        },
+                        labels
                       }
                     });
                   }}
-                  selectableLabels={selectableLabels}
-                />
-              )}
-            </Mutation>
-          </CardWrapper>
+                >
+                  {createCard => (
+                    <Card
+                      initialMode="creation"
+                      handleSubmit={values => {
+                        createCard({
+                          variables: {
+                            sentence: values.question,
+                            answer: values.response,
+                            clientMutationId: "mutationId"
+                          }
+                        });
+                      }}
+                      selectableLabels={selectableLabels}
+                    />
+                  )}
+                </Mutation>
+              </CardWrapper>
 
-          {cards.reverse().map(({ id, question, response }) => (
-            <CardWrapper key={`card-${id}`}>
-              <Card
-                initialMode="summary"
-                key={id}
-                question={question}
-                response={response}
-                labels={[{ name: "histoire" }]}
-                selectableLabels={selectableLabels}
-              />
-            </CardWrapper>
-          ))}
-        </React.Fragment>
-      );
-    }}
-  </Query>
-);
+              {cards.reverse().map(({ id, question, response }) => (
+                <CardWrapper key={`card-${id}`}>
+                  <Card
+                    initialMode="summary"
+                    key={id}
+                    question={question}
+                    response={response}
+                    labels={[{ name: "histoire" }]}
+                    selectableLabels={selectableLabels}
+                  />
+                </CardWrapper>
+              ))}
+            </React.Fragment>
+          );
+        }}
+      </Query>
+    );
+  }
+}
