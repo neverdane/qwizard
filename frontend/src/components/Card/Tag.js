91a479f9bd02e08411915893210aa6ecf8eca868
Alban Pommeret <ap@reputationvip.com>
2018-07-04 23:21:38 +0200

    Improve Tag components style

--- frontend/src/components/Card/Tag.js
+++ frontend/src/components/Card/Tag.js
@@ -4,14 +4,37 @@ import { Flex } from "grid-styled";
 
 const Container = Flex.extend`
   background-color: ${props => props.theme.colors.blue};
-  color: ${props => props.theme.colors.dark};
+  color: ${props => props.theme.colors.white};
   font-size: 0.8rem;
-  border-radius: 1em;
-  padding: 0 0.8em 0 0.8em;
+  padding: 0 0.7em 0 0.5em;
   position: relative;
-  margin: 0 0.6em 0 0;
+  margin: 0 0.6em 0 0.9em;
   align-items: center;
   line-height: 1.3rem;
+
+  &:before {
+    content: "";
+    position: absolute;
+    width: 1em;
+    left: -0.3em;
+    height: 100%;
+    display: flex;
+    background-color: ${props => props.theme.colors.blue};
+    z-index: -1;
+    transform: skewX(-20deg);
+  }
+
+  &:after {
+    content: "";
+    position: absolute;
+    width: 0.15em;
+    height: 100%;
+    display: flex;
+    background-color: ${props => props.theme.colors.blue};
+    z-index: -1;
+    transform: skewX(-20deg);
+    left: -0.6em;
+  }
 `;
 
 export const TagName = styled.span``;
