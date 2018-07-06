e42ec8f8176bc13b00fc1f5ee1a3acac9bf776ed
Alban Pommeret <ap@reputationvip.com>
2018-07-06 11:21:12 +0200

    Add an order filter by createdAt to Card entity

--- api/src/Entity/Card.php
+++ api/src/Entity/Card.php
@@ -2,7 +2,9 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -27,6 +29,7 @@ use App\Controller\QuestionResponse;
  *          }
  *     }
  * )
+ * @ApiFilter(OrderFilter::class, properties={"createdAt"})
  */
 class Card
 {
