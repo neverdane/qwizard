eb89cbf92d3b2e6ee0e3e15f24c038e4ef352b57
Alban Pommeret <ap@reputationvip.com>
2018-07-07 11:36:15 +0200

    Fix Tag height artifact

--- frontend/src/components/Card/Tag.js
+++ frontend/src/components/Card/Tag.js
@@ -1,23 +1,23 @@
 import React from "react";
-import styled from "styled-components";
 import { Flex } from "grid-styled";
 
 const Container = Flex.extend`
   background-color: ${props => props.theme.colors.blue};
   color: ${props => props.theme.colors.white};
   font-size: 0.8rem;
-  padding: 0.15em 0.7em 0.15em 0.5em;
+  padding: 0.15em 0.7em 0.15em 0.4em;
   position: relative;
   margin: 0 0.6em 0 0.9em;
   align-items: center;
-  line-height: 1.3em;
+  line-height: 1.5em;
   z-index: 0;
+  -webkit-backface-visibility: hidden;
 
   &:before {
     content: "";
     position: absolute;
     width: 1em;
-    left: -0.3em;
+    left: -0.4em;
     height: 100%;
     display: flex;
     background-color: ${props => props.theme.colors.blue};
@@ -34,14 +34,12 @@ const Container = Flex.extend`
     background-color: ${props => props.theme.colors.blue};
     z-index: -1;
     transform: skewX(-20deg);
-    left: -0.6em;
+    left: -0.7em;
   }
 `;
 
-export const TagName = styled.span``;
-
 export default ({ children }) => (
   <Container>
-    <TagName>{children}</TagName>
+    {children}
   </Container>
 );
