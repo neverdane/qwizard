555d9bc787dc3728401f67d1ab68c052070469bb
Alban Pommeret <ap@reputationvip.com>
2018-07-07 11:40:25 +0200

    Improve CardSummary display

--- frontend/src/components/Card/CardSummary.js
+++ frontend/src/components/Card/CardSummary.js
@@ -7,17 +7,31 @@ import { Trail } from "react-spring";
 
 const Title = styled.h3`
   color: ${props => props.theme.colors.dark};
-  font-size: 1.6rem;
+  font-size: 1.3rem;
+`;
+
+const Container = Flex.extend`
+  padding: 1em 0;
+  flex-grow: 1;
+  align-items: center;
+`;
+
+export const List = Flex.extend`
+  flex-direction: column;
+
+  ${Container} {
+    border-bottom: 0.05em solid rgba(0, 0, 0, 0.05);
+  }
 `;
 
 export default ({ question, labels = [] }) => {
   const rows = [
-    <React.Fragment>
+    <Container>
       <Title style={{ flexGrow: 1, marginRight: "1.3rem" }}>{question}</Title>
       <Flex>
         {labels.map(label => <Tag key={label.name}>{label.name}</Tag>)}
       </Flex>
-    </React.Fragment>
+    </Container>
   ];
 
   return (
