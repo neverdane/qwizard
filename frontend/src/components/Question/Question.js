d3500a01951da07c1f5fa2a3059d29ed7819c04a
Alban Pommeret <ap@reputationvip.com>
2018-07-29 19:35:19 +0200

    Animate Question component elements

--- frontend/src/components/Question/Question.js
+++ frontend/src/components/Question/Question.js
@@ -4,6 +4,8 @@ import { InversedInput } from "../Form/Input";
 import Button from "../Button/Button";
 import StreamLines from "../Stream/StreamLines";
 import theme from "../../theme";
+import { Transition, animated } from "react-spring";
+import Apparition, { ApparitionDiv } from "../utils/Apparition";
 
 export const STATUS_ANSWERING = "answering";
 export const STATUS_WRONG_ANSWER = "wrong-answer";
@@ -46,13 +48,13 @@ const Body = styled.div`
   color: ${props => props.theme.colors.white};
 `;
 
-const Title = styled.h2`
+const Title = styled(animated.h2)`
   color: ${props => props.theme.colors.white};
   align-self: flex-end;
   font-size: 1.8em;
 `;
 
-const Number = styled.span`
+const Number = styled(animated.span)`
   position: absolute;
   left: 0.5em;
   bottom: -0.5em;
@@ -60,7 +62,7 @@ const Number = styled.span`
   color: ${props => props.theme.colors.transparentDark};
 `;
 
-const QuestionRow = styled.div`
+const QuestionRow = styled(animated.div)`
   display: flex;
   flex-grow: 1;
   width: 100%;
@@ -97,53 +99,79 @@ const RightAnswer = styled.span`
   top: 0.4em;
 `;
 
+const PlaceholderContainer = styled(ApparitionDiv)`
+  width: 100%;
+`;
+
 export default ({ number, question, status, answer }) => (
   <Container>
     <Header status={status}>
-      <Number>{number.toString().padStart(2, "0")}</Number>
-      <Title>{question}</Title>
+      <Apparition>
+        {style => (
+          <Number style={style}>{number.toString().padStart(2, "0")}</Number>
+        )}
+      </Apparition>
+      <Apparition from={{ opacity: 0, x: -10 }} delay={200}>
+        {style => <Title style={style}>{question}</Title>}
+      </Apparition>
     </Header>
     <Body>
-      <QuestionRow>
-        <InversedInput
-          disabled={[STATUS_WRONG_ANSWER, STATUS_RIGHT_ANSWER].includes(status)}
-          style={{ flexGrow: 1, marginRight: "1em" }}
-        />
-        <Button
-          invertedColor={theme.colors.dark}
-          disabled={[STATUS_WRONG_ANSWER, STATUS_RIGHT_ANSWER].includes(status)}
-        >
-          Answer
-        </Button>
-      </QuestionRow>
+      <Apparition from={{ opacity: 0, x: 5 }} delay={200}>
+        {style => (
+          <QuestionRow style={style}>
+            <InversedInput
+              disabled={[STATUS_WRONG_ANSWER, STATUS_RIGHT_ANSWER].includes(
+                status
+              )}
+              style={{ flexGrow: 1, marginRight: "1em" }}
+            />
+            <Button
+              invertedColor={theme.colors.dark}
+              disabled={[STATUS_WRONG_ANSWER, STATUS_RIGHT_ANSWER].includes(
+                status
+              )}
+            >
+              Answer
+            </Button>
+          </QuestionRow>
+        )}
+      </Apparition>
       <Status>
         {status === STATUS_RIGHT_ANSWER && (
-          <RightAnswerStatus>Correct !</RightAnswerStatus>
+          <ApparitionDiv from={{ opacity: 0, x: -30 }}>
+            <RightAnswerStatus>Correct !</RightAnswerStatus>
+          </ApparitionDiv>
         )}
         {status === STATUS_WRONG_ANSWER && (
           <React.Fragment>
-            <WrongAnswerStatus>Faux...</WrongAnswerStatus>
-            <RightAnswer>La réponse était {answer}.</RightAnswer>
+            <ApparitionDiv from={{ opacity: 0, x: -30 }}>
+              <WrongAnswerStatus>Faux...</WrongAnswerStatus>
+            </ApparitionDiv>
+            <ApparitionDiv from={{ opacity: 0, x: -10 }} delay={400}>
+              <RightAnswer>La réponse était {answer}.</RightAnswer>
+            </ApparitionDiv>
           </React.Fragment>
         )}
         {status === STATUS_ANSWERING && (
-          <Placeholder
-            viewBox={`0 0 1920 100`}
-            lines={[
-              {
-                paths: [
-                  { width: 500 },
-                  { width: 300 },
-                  { width: 200 },
-                  { width: 400 },
-                  { width: 240 }
-                ]
-              },
-              {
-                paths: [{ width: 300 }, { width: 100 }, { width: 200 }]
-              }
-            ]}
-          />
+          <PlaceholderContainer from={{ opacity: 0, x: -3 }} delay={400}>
+            <Placeholder
+              viewBox={`0 0 1920 100`}
+              lines={[
+                {
+                  paths: [
+                    { width: 500 },
+                    { width: 300 },
+                    { width: 200 },
+                    { width: 400 },
+                    { width: 240 }
+                  ]
+                },
+                {
+                  paths: [{ width: 300 }, { width: 100 }, { width: 200 }]
+                }
+              ]}
+            />
+          </PlaceholderContainer>
         )}
       </Status>
     </Body>
