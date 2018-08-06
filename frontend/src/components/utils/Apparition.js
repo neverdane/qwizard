554c76b7fdd3b3da336f284ef33af2acdb47ff30
Alban Pommeret <ap@reputationvip.com>
2018-08-06 13:42:20 +0200

    Fix an animation glitch when switching between states

--- frontend/src/components/utils/Apparition.js
+++ frontend/src/components/utils/Apparition.js
@@ -1,27 +1,31 @@
 import React from "react";
 import { Transition, animated } from "react-spring";
 
-const Apparition = ({ from, children, delay }) => (
+const Apparition = ({ from, children, delay, keys=['default'] }) => (
   <Transition
     native
+    keys={keys}
     from={from || { opacity: 0, x: 50 }}
     enter={{ opacity: 1, x: 0 }}
     config={{ tension: 280, friction: 120 }}
     delay={delay}
   >
-    {({ x, opacity }) =>
-      children({
-        opacity,
-        transform: x.interpolate(x => `translate3d(${x}%,0,0)`)
-      })
-    }
+    {[
+      ({ x, opacity }) =>
+        children({
+          opacity,
+          transform: x.interpolate(x => `translate3d(${x}%,0,0)`)
+        })
+    ]}
   </Transition>
 );
 
 export default Apparition;
 
-export const ApparitionDiv = ({ children, className, ...props }) => (
-  <Apparition {...props}>
-    {style => <animated.div style={style} className={className} children={children} />}
+export const ApparitionDiv = ({ children, className, ...props, keys=['default'] }) => (
+  <Apparition {...props} keys={keys}>
+    {style => (
+      <animated.div key={keys[0]} style={style} className={className} children={children} />
+    )}
   </Apparition>
 );
