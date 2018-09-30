901d5b47a095f14ec6ee1b417c87c3c9be442b16
Alban Pommeret <ap@reputationvip.com>
2018-09-30 13:10:43 +0200

    Allow to override Apparition config

--- frontend/src/components/utils/Apparition.js
+++ frontend/src/components/utils/Apparition.js
@@ -1,20 +1,20 @@
 import React from "react";
 import { Transition, animated } from "react-spring";
 
-const Apparition = ({ from, children, delay, keys=['default'] }) => (
+const Apparition = ({ from, children, delay, config, keys=['default'] }) => (
   <Transition
     native
     keys={keys}
     from={from || { opacity: 0, x: 50 }}
     enter={{ opacity: 1, x: 0 }}
-    config={{ tension: 280, friction: 120 }}
+    config={config || { tension: 280, friction: 120 }}
     delay={delay}
   >
     {[
       ({ x, opacity }) =>
         children({
           opacity,
-          transform: x.interpolate(x => `translate3d(${x}%,0,0)`)
+          transform: x.interpolate(x => `translate3d(${x}%,0,0)`),
         })
     ]}
   </Transition>
